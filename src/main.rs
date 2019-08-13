32a6827305d576f9ad47c4475385353c3bfecef2
Dakota James Owen of the family Keeler <bearzrobotics@gmail.com>
2019-08-12 21:13:23 -0400

    init
    Yeah we can print stuff to the screen

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![no_std] // disable standard lib
+#![no_main] // disable fn main and c runtime. Must supply our own _start
+
+
+use core::panic::PanicInfo;
+
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello Wolrd!";
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    
+    loop {}
+}
